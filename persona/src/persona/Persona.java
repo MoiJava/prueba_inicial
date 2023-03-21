1c220f22db8f36cd31c01e66762d3ed8b6e464ec
MoiJava <mmascunyand@ausiasmarch.net>
2023-03-21 09:59:43 +0100

    primer cambio
    Signed-off-by: MoiJava <mmascunyand@ausiasmarch.net>

---
+++ persona/src/persona/Persona.java
@@ -0,0 +1,30 @@
+package persona;
+
+public class Persona {
+	
+	int edad;
+	double altura;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Persona obj1, obj2;
+		
+		obj1=new Persona();
+		obj2=new Persona();
+		
+		obj1.edad=30;
+		obj1.altura=1.75;
+		obj2.edad=20;
+		obj2.altura=1.80;
+		
+		System.out.println(obj1.edad);
+		System.out.println(obj1.altura);
+		System.out.println(obj2.edad);
+		System.out.println(obj2.altura);
+		
+		
+
+	}
+
+}
